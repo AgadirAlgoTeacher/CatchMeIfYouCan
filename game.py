5bc1407e64a0137cb37d51ba0a2f99b25b1e8ef8
AgadirAlgoTeacher <AgadirAlgoTeacher@proton.me>
2026-04-01 20:53:23 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -9,21 +9,28 @@ display.set_caption('Catch Me If You Can')
 
 background = transform.scale(image.load('Background.png'), Size)
 
-GuySize  = (150, 200 )
-CopSize = (200, 200)
+GuySize  = (100, 80 )
+CopSize = (100, 80)
+CarSize = (200, 150)
 
+#loading images
 Guy = transform.scale(image.load('Guy.png'), GuySize)
 Cop = transform.scale( image.load('Cop.png') , CopSize )
+car = transform.rotate(transform.scale(image.load('Car.png'), CarSize), 90)
 
-# Player properties
-GuyPosx = SCREEN_WIDTH // 2 
+
+# entities properties
+GuyPosx = 10 
 GuyPosy = SCREEN_HEIGHT // 2 
-GuySpeed = 5
-
+GuySpeed = 1
 
 CopPosx = 100
 CopPosy = 100
+CopSpeed = 0.5
 
+CarPosx = 350
+CarPosy = 200
+CarSpeed = 2
 
 game = True
 
@@ -45,11 +52,17 @@ while game:
         GuyPosy -= GuySpeed
     if keys[K_DOWN] and GuyPosy < SCREEN_HEIGHT - GuySize[1]:
         GuyPosy += GuySpeed
-        
+
+    #moving the car up and down
+    CarPosy += CarSpeed
+    if CarPosy <= 0 or CarPosy >= SCREEN_HEIGHT - 10:
+        CarSpeed *= -1
+
 
     window.blit(background, (0, 0))
     window.blit(Guy, (GuyPosx, GuyPosy))
     window.blit(Cop, (CopPosx, CopPosy))
-    # add the obstacles
+    window.blit(car, (CarPosx, CarPosy))
+    
     display.update()
 
