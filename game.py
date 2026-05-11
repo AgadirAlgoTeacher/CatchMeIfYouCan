b8ed53d34f453a85447332532eb3cd425643fb0f
AgadirAlgoTeacher <AgadirAlgoTeacher@proton.me>
2026-05-11 18:23:22 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -15,14 +15,23 @@ background = transform.scale(image.load('imgs/background.png'), Size)
 GuySize  = (SCREEN_WIDTH // 25, SCREEN_HEIGHT // 17)
 CopSize = (SCREEN_WIDTH // 20, SCREEN_HEIGHT // 16)
 CarSize = (SCREEN_WIDTH // 10, SCREEN_HEIGHT // 10)
-HUDSize = (SCREEN_WIDTH // 3, SCREEN_HEIGHT // 10)
+HUDSize = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 10)
 #loading images
 Guy = transform.scale(image.load('imgs/GuyIdle.png').convert_alpha(), GuySize)
-mouse.set_visible(False)
 gun = transform.scale(image.load('imgs/gun.png').convert_alpha(), (30, 30))
 Cop = transform.scale( image.load('imgs/CopIdle.png').convert_alpha() , CopSize )
-car = transform.rotate(transform.scale(image.load('imgs/Car.png').convert_alpha(), CarSize), 90)
-HUD = transform.scale(image.load('imgs/HUD.png').convert_alpha(), HUDSize)  
+Car = transform.rotate(transform.scale(image.load('imgs/Car.png').convert_alpha(), CarSize), 90)
+Health = transform.scale(image.load('imgs/Health.png').convert_alpha(), HUDSize)  
+Tazer = transform.scale(image.load('imgs/Tazer.png').convert_alpha(), HUDSize)
+Money = transform.scale(image.load('imgs/Money.png').convert_alpha(), HUDSize)
+
+mouse.set_visible(False)
+
+#game variables
+tazer = 5
+Health = 100
+Money = 0
+font = font.SysFont(None, 40)
 
 # entities properties
 GuyPosx = 10 
@@ -74,27 +83,39 @@ while game:
     if keys[K_RIGHT] and keys[K_UP]:
         angle = 45
 
-    #moving the car up and down
+    #moving the Car up and down
     CarPosy += CarSpeed
     if CarPosy <= 0 or CarPosy >= SCREEN_HEIGHT - 10:
         CarSpeed *= -1
     
-    # cop follows guy
-    if CopPosx < GuyPosx: CopPosx += CopSpeed; cop_angle = 0
-    if CopPosx > GuyPosx: CopPosx -= CopSpeed; cop_angle = 180
-    if CopPosy < GuyPosy: CopPosy += CopSpeed; cop_angle = -90
-    if CopPosy > GuyPosy: CopPosy -= CopSpeed; cop_angle = 90
-    # cop angle
-    if CopPosx < GuyPosx and CopPosy < GuyPosy: cop_angle = -45
-    if CopPosx < GuyPosx and CopPosy > GuyPosy: cop_angle = 45
-    if CopPosx > GuyPosx and CopPosy < GuyPosy: cop_angle = -135
-    if CopPosx > GuyPosx and CopPosy > GuyPosy: cop_angle = 135
-
+    # Cop follows guy
+    if CopPosx < GuyPosx: CopPosx += CopSpeed; Cop_angle = 0
+    if CopPosx > GuyPosx: CopPosx -= CopSpeed; Cop_angle = 180
+    if CopPosy < GuyPosy: CopPosy += CopSpeed; Cop_angle = -90
+    if CopPosy > GuyPosy: CopPosy -= CopSpeed; Cop_angle = 90
+    # Cop angle
+    if CopPosx < GuyPosx and CopPosy < GuyPosy: Cop_angle = -45
+    if CopPosx < GuyPosx and CopPosy > GuyPosy: Cop_angle = 45
+    if CopPosx > GuyPosx and CopPosy < GuyPosy: Cop_angle = -135
+    if CopPosx > GuyPosx and CopPosy > GuyPosy: Cop_angle = 135
+    
     window.blit(background, (0, 0))
     window.blit(transform.rotate(Guy, angle), (GuyPosx, GuyPosy))
     window.blit(gun, mouse.get_pos())
-    window.blit(transform.rotate(Cop, cop_angle), (CopPosx, CopPosy))
-    window.blit(car, (CarPosx, CarPosy))
-    window.blit(HUD, (0, SCREEN_HEIGHT - HUDSize[1]))
+    window.blit(transform.rotate(Cop, Cop_angle), (CopPosx, CopPosy))
+    window.blit(Car, (CarPosx, CarPosy))
+    # window.blit(HUD, (0, SCREEN_HEIGHT - HUDSize[1]))
+
+
+    Guy_rect = Guy.get_rect(topleft=(GuyPosx, GuyPosy))
+    Cop_rect = Cop.get_rect(topleft=(CopPosx, CopPosy))
+    Car_rect = Car.get_rect(topleft=(CarPosx, CarPosy))
+
+    if Guy_rect.colliderect(Cop_rect):
+        print('hit by Cop!')
+    if Guy_rect.colliderect(Car_rect):
+        print('hit by Car!')
+
+
     display.update()
 
