f34283a4201170261432d9139df9a0183c0b03b8
AgadirAlgoTeacher <AgadirAlgoTeacher@proton.me>
2026-05-13 20:31:02 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -3,30 +3,35 @@ from random import randint
 
 init()
 
-SCREEN_WIDTH = display.Info().current_w //1.05
-SCREEN_HEIGHT = display.Info().current_h //1.05
+SCREEN_WIDTH = int(display.Info().current_w //1.05)
+SCREEN_HEIGHT = int(display.Info().current_h //1.05)
 
-Size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+ScreenSize = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
-window = display.set_mode(Size)
+window = display.set_mode(ScreenSize)
 display.set_caption('Catch Me If You Can')
 
-background = transform.scale(image.load('imgs/background.png'), Size)
+background = transform.scale(image.load('imgs/background.png'), ScreenSize)
 
 GuySize  = (SCREEN_WIDTH // 25, SCREEN_HEIGHT // 17)
 CopSize = (SCREEN_WIDTH // 20, SCREEN_HEIGHT // 16)
 CarSize = (SCREEN_WIDTH // 10, SCREEN_HEIGHT // 10)
 IconSize = (SCREEN_WIDTH // 13, SCREEN_HEIGHT // 10)
+CashSize = (SCREEN_WIDTH // 35, SCREEN_HEIGHT // 25)
 
 #loading images
 Guy = transform.scale(image.load('imgs/GuyIdle.png').convert_alpha(), GuySize)
 gun = transform.scale(image.load('imgs/gun.png').convert_alpha(), (30, 30))
 Cop = transform.scale( image.load('imgs/CopIdle.png').convert_alpha() , CopSize )
 Car = transform.rotate(transform.scale(image.load('imgs/Car.png').convert_alpha(), CarSize), 90)
+Cash = transform.scale(image.load('imgs/Cash.png').convert_alpha(), CashSize)
 HealthImg  = transform.scale(image.load('imgs/Health.png').convert_alpha(), IconSize)  
 TazerImg = transform.scale(image.load('imgs/Tazer.png').convert_alpha(), IconSize)
 MoneyImg = transform.scale(image.load('imgs/Money.png').convert_alpha(), IconSize)
 TopScoreImg = transform.scale(image.load('imgs/TopScore.png').convert_alpha(), IconSize)
+SpawnedMoney = []
+for i in range(5):
+    SpawnedMoney.append((randint(0, int(SCREEN_WIDTH)-50), randint(0, int(SCREEN_HEIGHT)-50)))
 mouse.set_visible(False)
 
 #game variables
@@ -111,7 +116,7 @@ while game:
 
     if Guy_rect.colliderect(Cop_rect) or Guy_rect.colliderect(Car_rect):
         window.blit(background, (randint(-5, 5), randint(-5, 5)))
-        red = Surface(Size, SRCALPHA)
+        red = Surface(ScreenSize, SRCALPHA)
         red.fill((255, 0, 0, 80))
         window.blit(red, (0, 0))
     
@@ -131,11 +136,14 @@ while game:
     window.blit(font.render(str(TopScore), True, (255, 255, 255)), (IconSize[0] * 7 + 15, SCREEN_HEIGHT - IconSize[1] //1.5))  
     #Timer
     window.blit(font.render(f'Time: {int(time.get_ticks() / 1000)}s', True, (255, 255, 255)), (SCREEN_WIDTH - 150, 10))
-
-
-
-
-
+    
+    for pos in SpawnedMoney:
+        window.blit(Cash, pos)
+        if Guy_rect.colliderect(Cash.get_rect(topleft=pos)):
+            Money += 1
+            SpawnedMoney.remove(pos)
+            SpawnedMoney.append((randint(0, SCREEN_WIDTH-50), randint(0, SCREEN_HEIGHT-50)))
+            break
 
     display.update()
 
